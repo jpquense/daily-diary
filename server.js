c6e6167fe2f91f850b3ca14a55e963f15ab7d86d
John P Quense <jpquense@gmail.com>
2018-03-21 21:09:19 -0400

    added jwt and local auth strategies

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
-// const throng = require('throng');
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// const winston = require('winston');
 const mongoose = require('mongoose');
-// const { router: userRouter } = require('./routers/user.router');
+const passport = require('passport');
+
+const { router: usersRouter } = require('..routers/users/users.router');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./routers/auth.routers');
 const gratitudesRouter = require('./routers/gratitudes.router');
 const goalsRouter = require('./routers/goals.router');
 
@@ -14,10 +17,9 @@ const app = express();
 mongoose.Promise = global.Promise;
 
 const { DATABASE_URL, PORT } = require('./config/config');
-// const { PORT, DATABASE_URL, CONCURRENCY: WORKERS, ENV } = require('./config/main.config');
 
 app.use(morgan('common'));
-// Middlewares
+
 // CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -33,11 +35,6 @@ app.use(bodyParser.json());
 // Logging
 app.use(morgan('common'));
 
-// morgan.token('processId', () => process.pid);
-// if (ENV === 'development') {
-//   app.use(morgan(':processId - :method :url :status :response-time ms - :res[content-length]'));
-// }
-
 // Static Files
 app.use(express.static('public'));
 
@@ -45,87 +42,42 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/login/login.html');
 });
 
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
 // Routes
-// app.use('/api', usersRouter);
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
 app.use('/api', gratitudesRouter);
 app.use('/api', goalsRouter);
 
-app.get('/status', (req, res) => {
-  res.json({ processId: process.pid });
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
 });
 
+// if endpoint does not exist
 app.use('*', function (req, res) {
     res.status(404).json({ message: 'Not Found' });
   });
 
-// Starting Scripts
-// let server;
-// function runServer(databaseUrl) {
-//     return new Promise((res, rej) => {
-//         mongoose.connect(databaseUrl, (err) => {
-//             if (err) {
-//                 return rej(err);
-//             }
-//             if (ENV === 'development') {
-//                 winston.info(`Connected to ${databaseUrl}`);
-//             } else {
-//                 winston.info('Connected to database');
-//             }
-//             server = app.listen(PORT, () => {
-//                 winston.info(`App is listening on port ${PORT}`);
-//                 winston.info(`App is running in ${ENV} environment`);
-//                 winston.info(`Worker process id: ${process.pid}`);
-//                 winston.info('=========================================');
-//                 res();
-//             })
-//             .on('error', (error) => {
-//                 mongoose.disconnect();
-//                 rej(error);
-//             });
-//             return server;
-//         });
-//     });
-// }
-
-// function closeServer() {
-//   return mongoose.disconnect().then(() => (
-//       new Promise((res, rej) => {
-//           winston.info('Closing server.');
-//           server.close((err) => {
-//               if (err) {
-//                   return rej(err);
-//               }
-//               return res();
-//           });
-//       })
-//   ));
-// }
-
-// if (require.main === module) {
-//   throng({
-//       workers: WORKERS,
-//       lifetime: Infinity,
-//   }, () => {
-//       runServer(DATABASE_URL).catch(err => winston.info(err));
-//   });
-// }
-
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
 let server;
 
-// this function connects to our database, then starts the server
-function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+function runServer() {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
+    mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
       if (err) {
         return reject(err);
       }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
+      server = app
+        .listen(PORT, () => {
+          console.log(`Your app is listening on port ${PORT}`);
+          resolve();
+        })
         .on('error', err => {
           mongoose.disconnect();
           reject(err);
@@ -134,8 +86,6 @@ function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   });
 }
 
-// this function closes the server, and returns a promise. we'll
-// use it in our integration tests later.
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
@@ -150,16 +100,8 @@ function closeServer() {
   });
 }
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 }
-
-
-// app.listen(process.env.PORT || 8080, () => {
-//     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-//   });
-  
 
 module.exports = { app, runServer, closeServer };
---
+++ users/users.index.js
@@ -0,0 +1,5 @@
+'use strict';
+const { User } = require('../models/users.models');
+const { router } = require('../routers/users.router');
+
+module.exports = { User, router };
